017f117f920d845e16d7133a43e433fcd07161f4
Brad Davies <varbrad@users.noreply.github.com>
2016-12-06 20:03:00 +0000

    Day 6 (Both Parts)

---
+++ day6/part1.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+
+var input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf-8', flag: 'r' }).split('\n')
+
+var counts = [
+  {}, {}, {}, {}, {}, {}, {}, {}
+]
+
+_.each(input, function (code, a) {
+  _.each(code, function (char, b) {
+    if (counts[b][char] === undefined) {
+      counts[b][char] = { char: char, count: 1}
+    } else {
+      counts[b][char].count++
+    }
+  })
+})
+
+counts = _.map(counts, function (pos, i) {
+  pos = _.values(pos)
+  pos.sort(function (a, b) { return a.count < b.count ? 1 : -1 })
+  return pos[0].char
+})
+
+console.log(_.flatten(counts).join(''))
